b00ac6769e3dc0d23786d2d8fa2301a1c22147cf
Nikolai Plath <der.el.kuku@gmail.com>
2021-03-13 09:03:45 -0500

    Update

--- assets/app.js
+++ assets/app.js
@@ -5,20 +5,18 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.scss');
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+require('./css/app.scss');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// start the Stimulus application
+import './bootstrap';
 
 const $ = require('jquery');
 
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
-
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
